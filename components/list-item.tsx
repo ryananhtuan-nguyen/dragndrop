7f0b763fdb62a0716b5662a87c4c0a3d4489851d
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-12 16:52:12 +1300

    style: added margin

--- components/list-item.tsx
+++ components/list-item.tsx
@@ -16,9 +16,13 @@ export const ListItem = ({ data, index }: ListItemProps) => {
         <li
           {...provided.draggableProps}
           ref={provided.innerRef}
-          className="w-[200px] select-none mb-2"
+          className="shrink-0 h-full w-[200px] select-none mb-2"
+          onClick={() => console.log('ITEM', data.id)}
         >
-          <div className="w-full rounded-md pb-2" {...provided.dragHandleProps}>
+          <div
+            className="w-full rounded-md pb-2 border border-black bg-[#f1f2f4]"
+            {...provided.dragHandleProps}
+          >
             <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
               {data.title}
             </div>
@@ -27,7 +31,7 @@ export const ListItem = ({ data, index }: ListItemProps) => {
                 <ol
                   ref={provided.innerRef}
                   {...provided.droppableProps}
-                  className="flex flex-col gap-y-2"
+                  className="flex flex-col gap-y-2 mt-2"
                 >
                   {data.cards.map((card, index) => (
                     <CardItem index={index} key={card.id} data={card} />
