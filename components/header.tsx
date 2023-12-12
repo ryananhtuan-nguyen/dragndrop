c9d809ef02a817c354dc91391e1d62a2226da995
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-12 15:02:15 +1300

    chore: header display

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="w-full h-10 bg-gray-300 flex items-center justify-center text-black">
+      Drag and Drop
+    </div>
+  )
+}
